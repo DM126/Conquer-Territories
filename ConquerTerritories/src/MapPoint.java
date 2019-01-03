b8f4275f14af3369863dc1de426c09a8c02a19cb
DM126 <danny386126@gmail.com>
2019-01-03 15:54:24 -0500

    Initial commit

---
+++ ConquerTerritories/src/MapPoint.java
@@ -0,0 +1,25 @@
+
+/**
+ * Represents an x,y coordinate on the map image.
+ *
+ */
+public class MapPoint
+{
+	private int x, y;
+	
+	public MapPoint(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX()
+	{
+		return x;
+	}
+	
+	public int getY()
+	{
+		return y;
+	}
+}
