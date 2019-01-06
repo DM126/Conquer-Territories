3349c9043efdfc67f95cbe4beb8cd2ba1d446696
DM126 <danny386126@gmail.com>
2019-01-05 22:51:56 -0500

    Refactored Main Menu
    Moved the settings from MainMenu to a new class, SettingsPanel. changed the JTextfields on the main menu to JSpinners

--- ConquerTerritories/src/dir/MenuMain.java
+++ ConquerTerritories/src/dir/MenuMain.java
@@ -6,88 +6,51 @@ import javax.swing.*;
 import javax.swing.event.*;
 
 public class MainMenu extends JPanel
-{
-	//max strength value the user can input
-	private final int MAX_STRENGTH = 10;
-	
+{	
 	//TODO: add options for random bonus attacks, strength based on number of provinces, strength based on province value, etc...
 	//TODO: maybe add a seperate settings screen?
 	
-	private JComboBox<Game> gameSelect; //Choose a map from Maps.txt
-	private JTextField attackerMax, defenderMax;
 	private JButton play;
 	private JButton selectTeams;
 	private JButton loadGame;
-	private JCheckBox allowDraws;
+	private SettingsPanel settingsPanel;
 	private ConquerFrame parent;
 	
 	public MainMenu(ConquerFrame parent)
 	{
 		this.parent = parent;
 		
-		JLabel gameSelectLabel = new JLabel("Select a map: ");
-		gameSelect = new JComboBox<Game>();
-		getGames();
-		
-		JPanel gameSelectPanel = new JPanel();
-		gameSelectPanel.add(gameSelectLabel);
-		gameSelectPanel.add(gameSelect);
-		gameSelectPanel.setPreferredSize(new Dimension(gameSelectLabel.getPreferredSize().width + gameSelect.getPreferredSize().width + 20,
-														gameSelect.getPreferredSize().height + 10));
+		JLabel title = new JLabel("Conquer Territories");
+		title.setFont(new Font("Arial", Font.BOLD, 32));
 		
 		selectTeams = new JButton("Select Teams");
 		selectTeams.setToolTipText("Combine countries into teams (optional)");
-		
-		play = new JButton("Play!");
 		
 		loadGame = new JButton("Load game");
 		loadGame.setToolTipText("Load a saved game");
 		File saveFile = new File("GameData.save");
 		loadGame.setEnabled(saveFile.exists());
-		//TODO: add functionality to delete a save?
+		//TODO: add functionality to delete a save
 		
-		attackerMax = new JTextField(3);
-		attackerMax.setText("3");
-		attackerMax.setToolTipText("The maximum amount of times the attacker can take provinces from the defender");
-		defenderMax = new JTextField(3);
-		defenderMax.setText("3");
-		defenderMax.setToolTipText("The maximum amount of times the defender can take provinces from the attacker");
-		
-		allowDraws = new JCheckBox("Allow draws?", true);
-		//TODO: maybe make a checkbox to remove randomness and just always use the maximum
+		play = new JButton("Play!");
+		play.setToolTipText("Start a new game with the chosen settings");
 		
 		ButtonListener listener = new ButtonListener();
 		play.addActionListener(listener);
 		selectTeams.addActionListener(listener);
 		loadGame.addActionListener(listener);
-		attackerMax.getDocument().addDocumentListener(listener);
-		defenderMax.getDocument().addDocumentListener(listener);
 		
-		JLabel title = new JLabel("Conquer Territories");
-		title.setFont(new Font("Arial", Font.BOLD, 32));
-		
-		JLabel attackerLabel = new JLabel("Maximum attack strength:");
-		JLabel defenderLabel = new JLabel("Maximum defense strength:");
-		
-		JPanel optionsPanel = new JPanel();
-		optionsPanel.add(gameSelectPanel);
-		optionsPanel.add(attackerLabel);
-		optionsPanel.add(attackerMax);
-		optionsPanel.add(defenderLabel);
-		optionsPanel.add(defenderMax);
-		optionsPanel.add(allowDraws);
-		
-		optionsPanel.setPreferredSize(new Dimension(gameSelectPanel.getPreferredSize().width + 10, 
-				attackerMax.getPreferredSize().height + defenderMax.getPreferredSize().height + allowDraws.getPreferredSize().height + gameSelectPanel.getPreferredSize().height + 20));
+		settingsPanel = new SettingsPanel(this);
 		
 		JPanel uiPanel = new JPanel();
-		uiPanel.add(optionsPanel);
+		uiPanel.add(settingsPanel);
 		uiPanel.add(selectTeams);
 		uiPanel.add(loadGame);
 		uiPanel.add(play);
 		
-		uiPanel.setPreferredSize(new Dimension(optionsPanel.getPreferredSize().width, 
-												optionsPanel.getPreferredSize().height + play.getPreferredSize().height * 2 + 20));
+		uiPanel.setPreferredSize(new Dimension(settingsPanel.getPreferredSize().width, 
+												settingsPanel.getPreferredSize().height + play.getPreferredSize().height * 2 + 20));
+		
 		add(title);
 		add(uiPanel);
 		
@@ -101,66 +64,41 @@ public class MainMenu extends JPanel
 	 */
 	private void showErrorMessage()
 	{
-		JOptionPane.showMessageDialog(this, "Input must be a whole number from 0 to " + MAX_STRENGTH, "Invalid Input", JOptionPane.OK_OPTION);
+		JOptionPane.showMessageDialog(this, "Attack strength must be greater than 0 and defense strength", "Invalid Input", JOptionPane.OK_OPTION);
 	}
 	
 	/**
-	 * Find all the playable maps
+	 * Enables or disables the play button. To be used if invalid input is
+	 * detected in the settings.
+	 * 
+	 * @param enabled true if the buttons should be enabled, false otherwise
 	 */
-	private void getGames()
+	public void setButtonsEnabled(boolean enabled)
 	{
-		File dataFile = new File("Maps.txt");
-		
-		try
-		{
-			Scanner fileScan = new Scanner(dataFile);
-			Game game;
-			while (fileScan.hasNext())
-			{
-				game = new Game(fileScan.nextLine());
-				gameSelect.addItem(game);
-			}
-			
-			fileScan.close();
-		} 
-		catch (FileNotFoundException e)
-		{
-			JOptionPane.showMessageDialog(null, "Could not find Maps.txt.", "Error", JOptionPane.ERROR_MESSAGE);
-			System.exit(1);
-		}
+		play.setEnabled(enabled);
+		selectTeams.setEnabled(enabled);
 	}
 	
-	private class ButtonListener implements ActionListener, DocumentListener
+	private class ButtonListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent event)
 		{
 			if (event.getSource() == play || event.getSource() == selectTeams)
 			{
-				try
+				Settings settings = settingsPanel.getSettings();
+				
+				if (settings.getAttackerMax() > 0 && settings.getAttackerMax() >= settings.getDefenderMax())
 				{
-					int attackerMaximum = Integer.parseInt(attackerMax.getText());
-					int defenderMaximum = Integer.parseInt(defenderMax.getText());
-					
-					//Note: attacker maximum must be greater than 0, but defender maximum can equal 0.
-					if (attackerMaximum > 0 && attackerMaximum <= MAX_STRENGTH && defenderMaximum >= 0 && defenderMaximum <= MAX_STRENGTH)
+					if (event.getSource() == play)
 					{
-						Game game = (Game)gameSelect.getSelectedItem();
-						Settings settings = new Settings(attackerMaximum, defenderMaximum, allowDraws.isSelected(), game);
-						if (event.getSource() == play)
-						{
-							parent.startGame(settings);
-						}
-						else if (event.getSource() == selectTeams)
-						{
-							parent.selectTeams(settings);
-						}
+						parent.startGame(settings);
 					}
-					else
+					else if (event.getSource() == selectTeams)
 					{
-						showErrorMessage();
+						parent.selectTeams(settings);
 					}
 				}
-				catch (NumberFormatException ex)
+				else
 				{
 					showErrorMessage();
 				}
@@ -170,22 +108,5 @@ public class MainMenu extends JPanel
 				parent.loadGame();
 			}
 		}
-
-		//Enables the play button if the user types something.
-		public void insertUpdate(DocumentEvent e)
-		{
-			play.setEnabled(true);
-		}
-
-		//Disables the play button if the user empties a text field.
-		public void removeUpdate(DocumentEvent e)
-		{
-			if (e.getDocument().getLength() < 1)
-			{
-				play.setEnabled(false);
-			}
-		}
-		
-		public void changedUpdate(DocumentEvent e) {}
 	}
 }
